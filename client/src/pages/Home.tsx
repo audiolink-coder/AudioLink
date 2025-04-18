d6f8cb91b64fc4a7270330fdf227f9e360c69765
audiolink-coder <ritry0502@gmail.com>
2025-04-18 15:00:02 +0100

    Add files via upload

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import BotControlPanel from "@/components/BotControlPanel";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[#36393f] text-[#dcddde] font-sans">
+      <BotControlPanel />
+    </div>
+  );
+}
